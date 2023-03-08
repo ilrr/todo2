c4a7d0efa559865fa42b05b3029c85ea40d88e30
Ilari <ilari.rauhala@gmail.com>
2023-03-09 00:25:23 +0200

    changed developement api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 export const baseUrl = process.env.NODE_ENV === 'development'
-  ? 'http://192.168.1.120:3001/api'
+  ? `${/(.*):\d/.exec(window.location.href)[1]}:3001/api` // 'http://192.168.1.120:3001/api'
   : '/api';
 
 let token = null;
