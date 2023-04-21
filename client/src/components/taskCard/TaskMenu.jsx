159f23623810d57ce9088212f1b8279f84e83f46
Ilari <ilari.rauhala@gmail.com>
2023-04-21 15:13:33 +0300

    some menu changes

--- client/src/components/taskCard/TaskMenu.jsx
+++ client/src/components/taskCard/TaskMenu.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './TaskMenu.css';
 
 const TaskMenu = props => {
@@ -6,10 +6,11 @@ const TaskMenu = props => {
     setCopy, setUpdate, setMove, setShowMenu, setShowDeleteConfirmation, setAddChildren,
   } = props;
 
+  const [childTaskMenuExpanded, setChildTaskMenuExpanded] = useState(false);
   let clickedSomewhere = false;
 
-  const clickListener = () => {
-    if (clickedSomewhere) {
+  const clickListener = event => {
+    if (clickedSomewhere && !event.target.classList.contains('expandable')) {
       setShowMenu(false);
       document.removeEventListener('click', clickListener);
     } else
@@ -25,7 +26,19 @@ const TaskMenu = props => {
     <div className='task-menu-content'>
       <div className='task-menu-content-item' onClick={() => setUpdate(true)}> muokkaa </div>
       <div className='task-menu-content-item' onClick={() => setCopy(true)}> kopioi </div>
-      <div className='task-menu-content-item' onClick={() => setAddChildren(true)}> johdannaistehtävät </div>
+      {/* <div
+        className='task-menu-content-item'
+        onClick={() => setAddChildren(true)}>
+        johdannaistehtävät </div> */}
+      <div className='task-menu-content-item expandable' onClick={() => setChildTaskMenuExpanded(!childTaskMenuExpanded)}>
+        alatehtävät
+        {childTaskMenuExpanded && <>
+          <div className='task-menu-submenu'>
+            <div className='submenu-item' onClick={() => setAddChildren(true)}> lisää alatehtäviä </div>
+            <div className='submenu-item' onClick={() => console.log('nappi ;-)')}> muuta alatehtäväksi </div>
+          </div>
+          </>}
+      </div>
       <div className='task-menu-content-item' onClick={() => setMove(true)}> siirrä </div>
       <div className='task-menu-content-item' onClick={() => setShowDeleteConfirmation(true)}> poista </div>
     </div>
